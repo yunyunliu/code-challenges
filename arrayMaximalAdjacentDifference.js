f5c4c07f99ffd081bb07b9b268a5a4349cddd332
Yunyun Liu <celeph4is@gmail.com>
2021-12-12 14:09:45 -0800

    initial commit

---
+++ arrayMaximalAdjacentDifference.js
@@ -0,0 +1,8 @@
+// Given an array of integers, find the maximal absolute difference
+// between any two of its adjacent elements.
+// ex. input: [2, 4, 1, 0], output: 3
+// input constraints: 3 ≤ inputArray.length ≤ 10, -15 ≤ inputArray[i] ≤ 15.
+
+function solution(inputArray) {
+
+}
