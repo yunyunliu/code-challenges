9d5c17c28eff344bdeddac980a94277e7936bb79
Yunyun Liu <celeph4is@gmail.com>
2022-01-17 19:52:14 -0800

    implement shift and unshift methods of linkedlist data structure

--- dataStructures/singlyLinkedList/singlyLinkedList.js
+++ dataStructures/singlyLinkedList/singlyLinkedList.js
@@ -12,8 +12,8 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
-
-  push(val) { // push = add to end
+// methods
+  push(val) { // push = add node to end
     const newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
@@ -25,8 +25,66 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  pop() { // remove node from end of list and return removed node
+    if (this.length === 0) {
+      return;
+    }
+    let prev = this.head; // will become the new tail
+    let current = prev.next;
+
+    // traverse list until second-to-last node, set it's next property null, set this.tail to equal second-to-last node
+    while(current.next) {
+      prev = current;
+      current = current.next
+    }
+    prev.next = null;
+    this.tail = prev;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+
+  shift() { // remove node from front of linked list, and return it
+    if (!this.head) {
+      return;
+    }
+    const removed = this.head;
+    this.head = this.head.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return removed;
+  }
+
+  unshift(val) { // add new node to beginning of linked list
+    const newNode = new Node(val);
+    if (!list.head) { // if list is empty
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
 
 const list = new SinglyLinkedList(); // create empty list
 list.push('hello')
 list.push('there')
+list.push('you')
+list.push('!')
+list.unshift('there')
+list.unshift('hey')
+
+// console.log(list)
+
+
+// console.log('expect 0:', list.length)
+console.log(list)
